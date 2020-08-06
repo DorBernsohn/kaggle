c4558fa9e41ac3b066c0e0574a5c02cec70a5512
Bernsohn, Dor <dorber3@gmail.com>
2020-08-06 17:18:09 +0300

    Add textAugmentation class

--- utils/text.py
+++ utils/text.py
@@ -3,8 +3,15 @@
 import re
 import emoji
 import string
+import warnings
+import gensim.downloader as api
 from nltk.corpus import wordnet
+from transformers import pipeline
 from googletrans import Translator
+
+warnings.filterwarnings(action='ignore',category=UserWarning,module='gensim')  
+warnings.filterwarnings(action='ignore',category=FutureWarning,module='gensim')
+warnings.filterwarnings(action='ignore',category=FutureWarning,module='transformers')
 
 def clean_text(text, remove_emojis=True, remove_numbers=True, remove_punc=True, remove_url=True, remove_spaces=True):
         """Clean the text
@@ -56,36 +63,74 @@ def create_tfds_dataset(texts, lables):
     ds = tf.data.Dataset.from_tensor_slices((texts, texts))
     return ds
 
-def get_translation(text, dest_lang):
-    """translate text using google API, for all the language list visit: https://py-googletrans.readthedocs.io/en/latest/
 
-    Args:
-        text (string/list): string or list of strings
-        dest_lang (satring): string that describe the desired language
 
-    Raises:
-        ValueError: error if the input is not string or list
 
-    Returns:
-        string\list: string or list of string of the translation
-    """    
-    translator = Translator()
-    if isinstance(text, list):
-        translation = translator.translate(text, dest=dest_lang)
-        translation_list = []
-        for item in  translation:
-            translation_list.append(item.text)
-        return translation_list
-    elif isinstance(text, str):
-        translation = translator.translate(text, dest=dest_lang)
-        return translation.text
-    else:
-        raise ValueError(f"{type(text)} provided, supporting types: {str} or {list}")
+class textAugmentation():
+    def __init__(self, gensim_model='glove-twitter-25'):
+        self.gensim_model = api.load(gensim_model)
+        self.transformers_nlp = pipeline('fill-mask')
 
-def get_synonym(text):
-  synonyms = []
-  for syn in wordnet.synsets(text):
-      for lm in syn.lemmas():
-              if lm.name().lower() != text:
-                synonyms.append(lm.name().lower())
-  return synonyms
+    def get_most_similar(self, word, topn=5):
+        """get most similar words
+
+        Args:
+            word (string): word to compare in the model
+            topn (int, optional): number of similar words to return. Defaults to 5.
+
+        Returns:
+            list: list of tuple (word, score)
+        """        
+        return self.gensim_model.most_similar(word, topn=topn)
+
+        """return mask language model output (for example 'This is <mask> cool' -> 'This is pretty cool')
+
+        Args:
+            text ([string]): a string to feed the MLM
+        """
+    def get_MLM(self, text):
+        """return mask language model output (for example 'This is <mask> cool' -> 'This is pretty cool')
+
+        Args:
+            text ([string]): a string to feed the MLM
+
+        Returns:
+            list: list of dictionaries {'score': float, 'sequence': string, 'token': int, 'token_str': string}
+        """        
+        return self.transformers_nlp(text)
+
+    @staticmethod
+    def get_synonym(text):
+        synonyms = []
+        for syn in wordnet.synsets(text):
+            for lm in syn.lemmas():
+                    if lm.name().lower() != text:
+                        synonyms.append(lm.name().lower())
+        return synonyms
+    
+    @staticmethod
+    def get_translation(text, dest_lang):
+        """translate text using google API, for all the language list visit: https://py-googletrans.readthedocs.io/en/latest/
+
+        Args:
+            text (string/list): string or list of strings
+            dest_lang (satring): string that describe the desired language
+
+        Raises:
+            ValueError: error if the input is not string or list
+
+        Returns:
+            string\list: string or list of string of the translation
+        """    
+        translator = Translator()
+        if isinstance(text, list):
+            translation = translator.translate(text, dest=dest_lang)
+            translation_list = []
+            for item in  translation:
+                translation_list.append(item.text)
+            return translation_list
+        elif isinstance(text, str):
+            translation = translator.translate(text, dest=dest_lang)
+            return translation.text
+        else:
+            raise ValueError(f"{type(text)} provided, supporting types: {str} or {list}")
